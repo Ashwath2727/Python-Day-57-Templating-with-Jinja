7128a66941c2b5e0f55aff3cfd1bcfc7f9d8c08f
Ashwath S <miniash123@outlook.com>
2025-12-04 11:30:50 +0530

    added basic jinja templating logic

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
+import datetime
+import random
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    random_number = random.randint(1, 10)
+    current_year = datetime.datetime.now().year
+    my_name = "Ashwath"
+    return render_template("index.html", num=random_number, year=current_year, name=my_name)
 
 if __name__ == '__main__':
     app.run(debug=True)
